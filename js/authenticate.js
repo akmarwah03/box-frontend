64c8acbd63f93c369c5cd7714f522b80d28d77fe
Ravinder Shokar <Ravindershokar@hotmaiil.com>
2022-01-06 13:33:48 -0800

    New html front end

---
+++ js/authenticate.js
@@ -0,0 +1,64 @@
+
+document.querySelector('#register .submit').addEventListener('click', async (e) => {
+    e.preventDefault();
+    console.log(e);
+    const values = getRegisterValues();
+    const tokens = await registerUser(values);
+})
+
+
+document.querySelector('#login .submit').addEventListener('click', (e) => {
+    e.preventDefault();
+    const values = getLoginValues();
+    const tokens = await loginUser(values);
+    console.log(tokens);
+    if (tokens) {
+        //save to local storage
+    }
+})
+
+
+const getRegisterValues = () => {
+    return {
+        name: document.querySelector('#register .name').value,
+        password: document.querySelector('#register .password').value,
+        email: document.querySelector('#register .email').value,
+    }
+}
+
+
+const getLoginValues = () => {
+    return {
+        password: document.querySelector('#login .password').value,
+        email: document.querySelector('#login .email').value,
+    }
+}
+
+
+const registerUser = async (values) => {
+    const response = await fetch('http://localhost:8000/auth/signup', {
+        body: values,
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    console.log('Register Response', response)
+    return await response.json();
+
+}
+
+
+
+const loginUser = async (values) => {
+    const response = await fetch('http://localhost:8000/auth/signin', {
+        body: values,
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    console.log('Login Response', response)
+
+    return await response.json();
+}
